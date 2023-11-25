da67180294a095f665a6b9d4f89ba23a3d24f32a
jaeuklim <jeuk007@skuniv.ac.kr>
2023-11-25 17:54:03 +0900

    2-6

--- src/main/java/com/example/sbbmission/answer/Answer.java
+++ src/main/java/com/example/sbbmission/answer/Answer.java
@@ -1,28 +1,27 @@
-package com.example.sbbmission;
+package com.example.sbbmission.answer;
 
+import com.example.sbbmission.question.Question;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.LocalDateTime;
-import java.util.List;
 
 @Getter
 @Setter
 @Entity
-public class Question {
+public class Answer {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
-    @Column(length = 200)
-    private String subject;
-
     @Column(columnDefinition = "TEXT")
     private String content;
 
+    @CreatedDate
     private LocalDateTime createDate;
 
-    @OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
-    private List<Answer> answerList;
+    @ManyToOne
+    private Question question;
 }
