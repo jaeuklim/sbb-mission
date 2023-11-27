3de322a05ec696968a4449afcab38cc939598dd7
jaeuklim <jeuk007@skuniv.ac.kr>
2023-11-27 18:42:13 +0900

    2-10

--- src/main/java/com/example/sbbmission/question/QuestionService.java
+++ src/main/java/com/example/sbbmission/question/QuestionService.java
@@ -1,9 +1,11 @@
 package com.example.sbbmission.question;
 
+import com.example.sbbmission.DataNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 @Service
@@ -14,4 +16,14 @@ public class QuestionService {
     public List<Question> getList(){
         return this.questionRepository.findAll();
     }
+
+    public Question getQuestion(Integer id){
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
+    }
+
 }
